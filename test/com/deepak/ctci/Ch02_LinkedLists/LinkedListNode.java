7101c120ff31b9bb6872006f0497da0a417317ab
Eli Smith <league@WTS14-2.attlocal.net>
2018-11-04 14:32:07 -0800

    linked list

--- test/com/deepak/ctci/Ch02_LinkedLists/LinkedListNode.java
+++ test/com/deepak/ctci/Ch02_LinkedLists/LinkedListNode.java
@@ -2,4 +2,10 @@ package com.deepak.ctci.Ch02_LinkedLists;
 
 public class LinkedListNode<T> {
 
+	public LinkedListNode<Integer> next;
+
+	public LinkedListNode(int i) {
+		// TODO Auto-generated constructor stub
+	}
+
 }
